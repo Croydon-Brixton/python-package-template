ad9e6c23ce9fd3116854a576bcf75eecac3ba6f7
Simon Mathis <simon.mathis@gmail.com>
2023-11-27 18:12:12 +0000

    custom: update author "Allen Institue > Simon Mathis"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,8 +23,8 @@ from my_package import VERSION, VERSION_SHORT  # noqa: E402
 # -- Project information -----------------------------------------------------
 
 project = "my-package"
-copyright = f"{datetime.today().year}, Allen Institute for Artificial Intelligence"
-author = "Allen Institute for Artificial Intelligence"
+copyright = f"{datetime.today().year}, Simon Mathis"
+author = "Simon Mathis"
 version = VERSION_SHORT
 release = VERSION
 
